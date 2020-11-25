35794850476448d78c8565edd6b31337466186b4
anthonysokolov <anthonysokolov20@gmail.com>
2020-11-24 22:53:18 -0500

    created models dir

--- utils/create_edges.py
+++ utils/create_edges.py
@@ -14,6 +14,6 @@ files = [f for f in p.iterdir() if str(f)[-3:]=="jpg"]
 
 for file in files:
     img = cv.imread(str(file),0)
-    edges = Image.fromarray(cv.Canny(img,100,200))
-    edges = PIL.ImageOps.invert(edges)
-    edges.save("../data/edges/edges-" + file.parts[-1])
+    edges = ~cv.Canny(img,0,255) # apply canny algorithm and invert image
+    cv.imwrite("../data/train/edges-" + file.parts[-1], edges)
+    #cv.imwrite("../data/train/"+file.parts[-1], img)
